a8446193c73334883772f02646b24eb9e568e626
Steve Brown <contactstevebrown@gmail.com>
2020-03-20 00:45:57 +0000

    Complete volunteer flow

--- src/app/complete/complete.page.ts
+++ src/app/complete/complete.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-complete',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompletePage implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.resetUser();
   }
 
 }
