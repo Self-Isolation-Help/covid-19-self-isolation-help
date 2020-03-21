26b048bc7783b321a4b9eb2f31ad2ba535241129
Steve Brown <contactstevebrown@gmail.com>
2020-03-21 13:04:48 +0000

    Support the scaling of the application to the rest of the UK, but start off with only Kingston and Richmond areas

---
+++ src/app/extend-operations/extend-operations.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extend-operations',
+  templateUrl: './extend-operations.page.html',
+  styleUrls: ['./extend-operations.page.scss'],
+})
+export class ExtendOperationsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
