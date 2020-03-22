4f77d73ab7f5bd7ec6cdec05c3454fd58bddb338
Paul Hanlon <paulhanlon21@hotmail.co.uk>
2020-03-22 21:06:02 +0000

    Limit isolating by working counties

--- src/app/people-isolating/people-isolating.page.ts
+++ src/app/people-isolating/people-isolating.page.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Isolator } from "../isolator.model";
 import { Observable } from "rxjs/internal/Observable";
-import { map } from "rxjs/operators";
+import { map, switchMap } from "rxjs/operators";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { Router } from "@angular/router";
+import { Volunteer } from '../volunteer';
 
 @Component({
   selector: "app-people-isolating",
@@ -13,21 +14,34 @@ import { Router } from "@angular/router";
 })
 export class PeopleIsolatingPage implements OnInit {
   isolators$: Observable<Array<Isolator>>;
+  user$: Observable<Volunteer>;
+
   constructor(
     private afs: AngularFirestore,
     public auth: AngularFireAuth,
     private router: Router
-  ) {}
+  ) {
+    this.user$ = this.auth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<Volunteer>(`user/${user.uid}`).valueChanges();
+        }
+      }));
+  }
 
   ngOnInit() {
-    this.isolators$ = this.afs
-      .collection<Isolator>("isolating")
-      .valueChanges()
-      .pipe(
-        map((isolators: any) => {
-          return isolators.filter(isolator => !isolator.resolved);
-        })
-      );
+    this.isolators$ = this.user$.pipe(
+      map(user => user.counties),
+      switchMap((counties) => {
+        return this.afs
+          .collection<Isolator>("isolating")
+          .valueChanges()
+          .pipe(
+            map((isolators: Isolator[]) => {
+              return isolators.filter(isolator => !isolator.resolved
+              && (counties.includes('*') || counties.includes(isolator.details.county)));
+            }));
+      }));
   }
 
   onSignout() {
