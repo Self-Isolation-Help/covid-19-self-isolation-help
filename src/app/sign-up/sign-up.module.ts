44bc3355239f08f8475cfcf926c70caeb44cdb1d
Paul Hanlon <paulhanlon21@hotmail.co.uk>
2020-03-24 18:43:24 +0000

    form validation, firestore auth

--- src/app/sign-up/sign-up.module.ts
+++ src/app/sign-up/sign-up.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 import { SignUpPageRoutingModule } from './sign-up-routing.module';
 
 import { SignUpPage } from './sign-up.page';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @NgModule({
   imports: [
@@ -15,6 +17,8 @@ import { SignUpPage } from './sign-up.page';
     IonicModule,
     SignUpPageRoutingModule
   ],
-  declarations: [SignUpPage]
+  declarations: [SignUpPage],
+  providers: [AngularFirestore,
+  AngularFireAuth]
 })
 export class SignUpPageModule {}
