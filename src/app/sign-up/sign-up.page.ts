114f2c11999f9dfa41d2bdfcfe305c451ca13ca8
Andrew Davies <andrew.davies@and.digital>
2020-03-24 07:58:45 +0000

    Added methods to ts, added drop downs for county select

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { COUNTIES } from "../counties";
+import { LOCATIONS } from "../locations";
+import { COUNTIES_LOCATION_MAP } from "../counties-location-map";
+import { SubdomainService } from "../subdomain.service";
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.page.html',
-  styleUrls: ['./sign-up.page.scss'],
+  selector: "app-sign-up",
+  templateUrl: "./sign-up.page.html",
+  styleUrls: ["./sign-up.page.scss"]
 })
 export class SignUpPage implements OnInit {
+  form: any = {};
+  counties: string[] = COUNTIES.sort();
+  locations = LOCATIONS.sort();
+  locationsAssociatedWithCounty;
+  countiesLocationMap = COUNTIES_LOCATION_MAP;
+  locationsList = [];
+  subdomain;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private subdomainService: SubdomainService
+  ) {}
 
   ngOnInit() {
-  }
+    this.subdomain = this.subdomainService.getLabelForSubdomain();
 
+    this.form.location = this.subdomainService.getLocationBySubdomain();
+    this.form.county = this.subdomainService.getCountyBySubdomain();
+    this.onChangeCounty();
+  }
+  onChangeCounty() {
+    this.locationsAssociatedWithCounty = this.countiesLocationMap.find(
+      map => map.county === this.form.county
+    );
+    if (this.locationsAssociatedWithCounty) {
+      this.locationsList = this.locations.find(
+        location =>
+          location.label === this.locationsAssociatedWithCounty.locations
+      ).items;
+      this.form.locationType = this.locationsAssociatedWithCounty.locations;
+    } else {
+      delete this.form.location;
+    }
+  }
+  onChangeTown() {
+    if (this.form.town.toLowerCase() === "london") {
+      this.form.county = "Greater London";
+    } else {
+      delete this.form.county;
+    }
+  }
 }
