9f6936f9929d8f5a9db601057c600557c1a8df6b
Steve Brown <contactstevebrown@gmail.com>
2020-03-25 22:09:33 +0000

    Admin Volunteer Approval

---
+++ src/app/approve-volunteers/approve-volunteers.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from "@angular/core";
+import { Isolator } from "../models/isolator.model";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs/internal/Observable";
+import { Volunteer } from "../models/volunteer";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AlertController } from "@ionic/angular";
+
+@Component({
+  selector: "app-approve-volunteers",
+  templateUrl: "./approve-volunteers.page.html",
+  styleUrls: ["./approve-volunteers.page.scss"]
+})
+export class ApproveVolunteersPage implements OnInit {
+  volunteers$: Observable<Volunteer>;
+  volunteersNeedApproval: Array<Volunteer>;
+  volunteersAlreadyApproved: Array<Volunteer>;
+
+  constructor(
+    private afs: AngularFirestore,
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit() {
+    this.volunteers$ = this.afs
+      .collection<Isolator>("volunteers")
+      .valueChanges({ idField: "id" })
+      .pipe(
+        map((volunteers: any) => {
+          return volunteers.filter(volunteer => volunteer.approved !== false && !volunteer.rejected);
+        })
+      );
+
+    this.volunteers$.subscribe((volunteers: any) => {
+      console.log(volunteers);
+      this.volunteersNeedApproval = volunteers.filter(
+          volunteer => volunteer.roles && (!volunteer.roles.admin && !volunteer.roles.volunteer)
+      );
+      this.volunteersAlreadyApproved = volunteers.filter(
+        volunteer => volunteer.roles && (volunteer.roles.admin || volunteer.roles.volunteer)
+      );
+    });
+  }
+}
