ba0835bcb12a65e89fbee17a77bff5e5a6807271
Andrew Davies <andrew.davies@and.digital>
2020-03-31 15:52:27 +0100

    added trigger and amended email functionality

--- src/app/utils/cloud-funcs-send-email/functions/index.js
+++ src/app/utils/cloud-funcs-send-email/functions/index.js
@@ -1,63 +1,39 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
 const nodemailer = require("nodemailer");
-const cors = require("cors")({ origin: true });
-admin.initializeApp();
 
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "volunteers@selfisolationhelp.co.uk",
-    pass: "yourgmailaccpassword"
-  }
-});
-
-exports.sendMail = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-      
-    //TODO check both args passed and populated before proceeding
-
-    const dest = req.query.dest;
-    const type = req.query.dest;
-    const key = key = functions.config().secure_token.key;
-    const requestKey = request.get('key');
-
-    if(key === requestKey)
-    {
-      const emailSubject;
-    const emailBody;
-    if(type === "1") // volunteer accept
-    {
-        emailSubject = 'Volunteer application ACCEPTED'
-        emailBody  =  `<p style="font-size: 16px;">You have been accepted as volunteer</p><br />
-        <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />`
-    }
-    
-    const mailOptions = {
-      from: "Your Account Name <yourgmailaccount@gmail.com>", // Something like: Jane Doe <janedoe@gmail.com>
-      to: dest,
-      subject: emailSubject,
-      html: emailBody
-    };
-
-    // returning result
-    return transporter.sendMail(mailOptions, (erro, info) => {
-      if (erro) {
-        return res.send(erro.toString());
-      }
-      return res.send("Sended");
-    });
-    }
-    else
-    {
-      //TODO invalid request - do not send email
+exports.emailMessage = args => {
+  const transport = nodemailer.createTransport({
+    service: "",
+    auth: {
+      user: "volunteers@selfisolationhelp.co.uk",
+      pass: " ! ! password goes here ! ! "
     }
   });
-});
+  const mailOptions = {
+    from: "volunteers@selfisolationhelp.co.uk",
+    to: args.email,
+    subject: "Isolation Help - Applicattion ACCEPTED",
+    html: "Need some text here........"
+  };
+  transport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      //TODO email failed
+    }
+    //TODO email success
+  });
+};
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.updatingVolunteer = functions.database
+  .ref("/volunteer/{id}")
+  .onUpdate((change, context) => {
+    const agreebefore = change.before.child("volunteer").val();
+    const agreeAfter = change.after.child("volunteer").val();
+    const email; //TODO get email for voluteer
+
+    if (
+      (agreebefore === null && agreeAfter === true) ||
+      (agreebefore === failse && agreeAfter == true)
+    ) {
+      this.emailMessage(email);
+    }
+  });
