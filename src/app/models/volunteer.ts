5249700694df8d137c4e13dd02ec1359df509676
Steve Brown <contactstevebrown@gmail.com>
2020-03-31 23:15:37 +0100

    Matching Service

--- src/app/models/volunteer.ts
+++ src/app/models/volunteer.ts
@@ -1,32 +1,32 @@
 import { VolunteerRole } from './volunteer-role.enum';
 
 export interface Volunteer {
-    details: {
-        forename: string;
+    details?: {
+        forename?: string;
         middleNames?: string;
-        surname: string;
-        telephone: string;
-        email: string;
-        address1: string;
+        surname?: string;
+        telephone?: string;
+        email?: string;
+        address1?: string;
         address2?: string;
-        town: string;
-        county: string;
-        postCode: string;
-        socialMediaLink: string;
+        town?: string;
+        county?: string;
+        postCode?: string;
+        socialMediaLink?: string;
     };
-    volunteerGroup: {
-        name: string;
-        link: string;
-        role: string;
+    volunteerGroup?: {
+        name?: string;
+        link?: string;
+        role?: string;
     };
-    checks: {
-        hasCriminalRecord: boolean;
-        dbsNumber: string;
-        disclaimerSigned: boolean;
-        gdprSigned: boolean;
+    checks?: {
+        hasCriminalRecord?: boolean;
+        dbsNumber?: string;
+        disclaimerSigned?: boolean;
+        gdprSigned?: boolean;
     };
-    workingCounties: string[];
-    workingLocations: string[];
+    workingCounties?: string[];
+    workingLocations?: string[];
     roles?: VolunteerRole[];
     dateCreated?: any;
     approved?: boolean;
