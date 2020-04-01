69be4eed380ffe19f35541b6ebea2c0cc18a2f8c
shukii <josh@joshjam.com>
2020-04-01 14:29:19 +0100

    test

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,9 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require("path").join(__dirname, "../coverage"),
-      reports: ["html", "lcovonly", "text-summary"],
+      reports: ["html",
+        "lcovonly",
+        "text-summary"],
       fixWebpackSourcePaths: true,
     },
     reporters: ["progress", "kjhtml"],
