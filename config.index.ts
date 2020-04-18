ee570a6b79d4ae3e36553ef87f60e54e5bfd7cf7
shukii <josh@joshjam.com>
2020-04-18 17:12:34 +0100

    added dev api key to test

--- config.index.ts
+++ config.index.ts
@@ -6,13 +6,13 @@ const envConfigFile = `export const environment = {
     production: true,
     firebase: {
       apiKey: '${process.env.FIREBASE_API_KEY}',
-      authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-      databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
-      projectId: '${process.env.FIREBASE_PROJECT_ID}',
-      storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-      messagingSenderId: '${process.env.FIREBASE_MESSAGINE_SENDER_ID}',
-      appId: '${process.env.FIREBASE_APP_ID}',
-      measurementId: '${process.env.FIREBASE_MEASUREMENT_ID}',
+      authDomain: "self-isolation-help-dev.firebaseapp.com",
+      databaseURL: "https://self-isolation-help-dev.firebaseio.com",
+      projectId: "self-isolation-help-dev",
+      storageBucket: "self-isolation-help-dev.appspot.com",
+      messagingSenderId: "1066821517284",
+      appId: "1:1066821517284:web:8d2e73a3c092a9877a91f1",
+      measurementId: "G-K3ZBBM27HL",
     },
 `;
 
