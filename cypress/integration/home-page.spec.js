a558b5fffe1e03f6c5f713947b9740cbab17a8c1
Andrew Davies <andrew.davies@and.digital>
2020-04-20 17:01:06 +0100

    corrected home page title test

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -3,7 +3,7 @@
 describe("Home Page", () => {
   it("should display the site name on the homepage", () => {
     cy.visit("/");
-    cy.get(".md card-content-md hydrated").should(
+    cy.get("h1").should(
       "contain.text",
       "COVID-19 Self-Isolation Help for the UK"
     );
